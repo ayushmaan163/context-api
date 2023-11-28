dfc49a664e9c1f4f5ff90522862745259c59f748
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-28 15:17:09 +0530

    Added more element

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import React from "react";
 import UserContextProvider from "./context/UserContextProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
       <UserContextProvider>
         <h1>Learning Context API</h1>
+        <Login />
+        <Profile />
       </UserContextProvider>
     </>
   );
